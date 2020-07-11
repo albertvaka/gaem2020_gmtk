e9d34d32b4f4b34baaef08387799d243d509c90a
Albert Vaca Cintora <albertvaka@gmail.com>
2020-07-11 14:34:15 +0200

    Comment prints

--- src/asteroid.cpp
+++ src/asteroid.cpp
@@ -50,17 +50,17 @@ void Asteroid::Update(float dt) {
 
       if (dist_bounds <= 0) {
         vec direction = (other->pos - pos).Normalized();
-        Debug::out << "dir: " << direction;
+        //Debug::out << "dir: " << direction;
         vec direction_other = (pos - other->pos).Normalized();
-        Debug::out << "dir_ot: " << direction_other;
+        //Debug::out << "dir_ot: " << direction_other;
 
         float own_vel = (mass - other->mass) * velocity.Length()
           / (mass + other->mass);
         float their_vel = (other->mass - mass) * other->velocity.Length()
           / (mass + other->mass);
-        Debug::out << "vel " << own_vel << " tvel " << their_vel;
+        //Debug::out << "vel " << own_vel << " tvel " << their_vel;
         velocity += direction * own_vel;
-        Debug::out << velocity;
+        //Debug::out << velocity;
         other->velocity += direction_other * their_vel;
       }
     }
